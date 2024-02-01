bec7710dc6a1225503ef853ce32372f60804d5ff
Virendra Jopale <virujopale@gmail.com>
2024-02-02 01:57:03 +0530

    third

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ const orders=require("./routes/orderRoute")
 const payment=require("./routes/paymentRoute")
 const bodyParser=require('body-parser')
 app.use(express.json())
+app.use(express.json({ limit: "50mb" })); app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(fileUpload())
